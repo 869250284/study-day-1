c034799d87a294eebfa9f8d9dda61b397f51cfa9
闫小明 <869250284@qq.com>
2020-09-15 10:25:03 +0800

    2020.9.15class

---
+++ 2020.9.15.c/2020.9.15.c/9.15.c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+#include<math.h>
+//main()
+//{
+//	double y;
+//	int x,m;
+//	for(y=10;y>=-10;y--)
+//	{
+//		m=2.5*sqrt(100-y*y);
+//		for(x=1;x<50-m;x++)
+//			printf(" ");
+//			printf("*");
+//			for(;x<50+m;x++);
+//			printf(" ");
+//			printf("*\n");
+//	}
+//}
+//int Ad(int x,int y,int r)
+//{
+//	/*将（x，y）转换为相当于圆心的坐标（r,r）*/
+//	int rx=x-r;
+//	int ry=y-r;
+//	double d=sqrt(rx*rx+ry*ry)-r;
+//	if(fabs(d)<0.5="")
+//}
+//void print(int r )
+//{
+//	int x,y;
+//	for(y=0;y<2*(r+1);)
+//	{
+//		for(x=0;x<2*(r+1);)
+//		{
+//			if(Ad(x,y,r))/*如果这个点在圆上*//*，打印*如果没在打印空格*/
+//		}
+//	}
+//}
+//int main()
+//{
+//	int r;//输入圆的半径
+//	printf("输入圆的半径\nr=");
+//	scanf("%d",&r);
+//	print(r);
+//	return 0;
+//}
+//int clam(int vaule,int m_min,int m_max)
+//{
+//	int mm_ret=vaule;
+//	if(vaule<m_min)
+//	{
+//		mm_ret=m_min;
+//	}
+//	else if(vaule>m_max)
+//	{
+//		mm_ret=m_max;
+//	}
+//	return mm_ret;
+//
+//}
+//int DS(unsigned char * data , int width,int height,int channel,int x,int y,int r int shinner,unsigned char red,unsigned char gree,unsigned char blue)
+//{
+//	unsigned char * tempUchar=data;
+//	int i=0;
+//	int j=0;
+//	int k=0;
+//
+//}
+//signal是一个函数声明
+//signal函数的参数有两个 第一个是int第二个是函数指针 该函数指针指向的函数的参数是int 返回类型是void
+//signal函数的返回类型也是一个函数指针该函数指针指向函数参数int，返回类型void
+//void (* signal(int,void(*)(int)))(int);
+////简化
+//typedef void(* prfun_t)(int);
+//prfun_t signal(int,prfun_t);
+//typedef unsigned int unint
+//
+//int Add(int x,int y)
+//{
+//	int z=0;
+//	z=x+y;
+//	return z;
+//}
+//int Sub(int x,int y)
+//{
+//	return x-y;
+//}
+//int Mu1(int x ,int y)
+//{
+//	return x*y;
+//}
+//int main()
+//{
+//	int *arr[5];
+//	int(*pa)(int ,int )=Add;
+//	return 0;
+//}
