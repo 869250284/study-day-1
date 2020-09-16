a5d8dd52b3b75e84fcea2a33c7e79e6be3c0bc46
闫小明 <869250284@qq.com>
2020-09-16 19:50:54 +0800

    九月十六日

---
+++ 2020.9.16.c/2020.9.16.c/9.16.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//void menu()
+//{
+//	printf("**************************\n");
+//	printf("*1.add***********2.sub****\n");
+//	printf("*3.mul***********4.div****\n");
+//	printf("********0.exit************\n");
+//	printf("**************************\n");
+//}
+//int Add(int x ,int y )
+//{
+//	return x+y;
+//}
+//int Sub(int x,int y)
+//{
+//	return x-y;
+//}
+//int mul(int x ,int y)
+//{
+//	return x*y;
+//}
+//int Div(int x ,int y)
+//{
+//	return x/y;
+//}
+//int main()
+//{
+//	int input =0;
+//	int x =0;
+//	int y=0;
+//	do
+//	{
+//		menu();
+//		printf("请选择>");
+//		scanf("%d",&input);
+//		printf("请输入两个数");
+//		scanf("%d%d",&x,&y);
+//		switch (input)
+//		{
+//		case 1:
+//			printf("请输入两个数");
+//			printf("%d\n",Add(x,y));
+//			break;
+//		case 2:
+//			printf("请输入两个数");
+//			printf("%d\n",Sub(x,y));
+//			break;
+//		case 3:
+//			printf("请输入两个数");
+//			printf("%d\n",mul(x,y));
+//			break;
+//		case 4:
+//			printf("请输入两个数");
+//			printf("%d\n",Div(x,y));
+//			break;
+//		case 0:
+//			printf("退出\n");
+//			break;
+//		default:
+//			printf("选择错误，请重新选择");
+//		}
+//	}while(input);
+//	return 0;
+//}
+//int main()
+//{
+//	int input =0;
+//	int x =0;
+//	int y=0;
+//	int (*p[5])(int .,int );
+//	return 0;
+//}
